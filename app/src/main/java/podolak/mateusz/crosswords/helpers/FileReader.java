7dc0168e9735ae67a91700869cfb4db19bca36ef
matpod <podolak.93@gmail.com>
2016-05-15 20:58:38 +0200

    Fixed FileReader method.

--- app/src/main/java/podolak/mateusz/crosswords/helpers/FileReader.java
+++ app/src/main/java/podolak/mateusz/crosswords/helpers/FileReader.java
@@ -12,13 +12,15 @@ import podolak.mateusz.crosswords.R;
 
 public class FileReader {
 
+    private String currentLine = null;
     private List<String> wordsList = new ArrayList<>();
 
-    //to invoke it use this from acticity: getResources().openRawResource(R.raw.words_file);
+    //to invoke it use this from activity: getResources().openRawResource(R.raw.words_file);
+    //or not from activity: getClass().getClassLoader().getResourceAsStream("res/raw/words_file");
     public List<String> getWordsFromFile(InputStream file) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(file));
-        while (bufferedReader.readLine() != null) {
-            wordsList.add(bufferedReader.readLine());
+        while ((currentLine = bufferedReader.readLine()) != null) {
+            wordsList.add(currentLine);
         }
         return wordsList;
     }
