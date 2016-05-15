6500edaab5b6d1452411bf2c0fac86f830a0bb79
matpod <podolak.93@gmail.com>
2016-05-15 21:38:54 +0200

    Added new methods to class GridSetup.java

--- app/src/main/java/podolak/mateusz/crosswords/helpers/GridSetup.java
+++ app/src/main/java/podolak/mateusz/crosswords/helpers/GridSetup.java
@@ -2,12 +2,14 @@ package podolak.mateusz.crosswords.helpers;
 
 public class GridSetup {
 
-    private int rows = 15;
-    private int columns = 15;
-    private char[][] grid = new char[rows][columns];
+    private int rowSize = 15;
+    private int columnSize = 15;
+    private char[][] grid = new char[rowSize][columnSize];
 
-    public GridSetup(char[][] grid) {
-        this.grid = grid;
+    public GridSetup(int rowSize, int columnSize) {
+        this.rowSize = rowSize;
+        this.columnSize = columnSize;
+        this.grid = new char[rowSize][columnSize];
     }
 
     public char[][] getGrid() {
@@ -18,20 +20,20 @@ public class GridSetup {
         this.grid = grid;
     }
 
-    public int getRows() {
-        return rows;
+    public int getRowSize() {
+        return rowSize;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
+    public void setRowSize(int rowSize) {
+        this.rowSize = rowSize;
     }
 
-    public int getColumns() {
-        return columns;
+    public int getColumnSize() {
+        return columnSize;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
+    public void setColumnSize(int columnSize) {
+        this.columnSize = columnSize;
     }
 
     public char getCell(int row, int column) {
@@ -42,7 +44,45 @@ public class GridSetup {
         this.grid[row][column] = cell;
     }
 
+    public char[] getRow(int row) {
+        char[] charsInRow = new char[this.columnSize];
+        for (int i = 0; i < this.columnSize - 1; i++) {
+            charsInRow[i] = this.grid[row][i];
+        }
+        return charsInRow;
+    }
+
+    public char[] getColumn(int column) {
+        char[] charsInColumn = new char[this.rowSize];
+        for (int i = 0; i < this.rowSize - 1; i++) {
+            charsInColumn[i] = this.grid[i][column];
+        }
+        return charsInColumn;
+    }
+
     public boolean isCellEmpty(int row, int column) {
         return getCell(row, column) == '\u0000';
+    }
+
+    public boolean isCharEmpty(char ch) {
+        return ch == '\u0000';
+    }
+
+    public boolean isRowEmpty(int row) {
+        char[] charsInRow = getRow(row);
+        for (char cell : charsInRow) {
+            if (!isCharEmpty(cell))
+                return false;
+        }
+        return true;
+    }
+
+    public boolean isColumnEmpty(int column) {
+        char[] charsInColumn = getRow(column);
+        for (char cell : charsInColumn) {
+            if (!isCharEmpty(cell))
+                return false;
+        }
+        return true;
     }
 }
